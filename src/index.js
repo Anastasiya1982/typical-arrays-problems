0417323c98e6018055f67a54a96679e4a7393e98
ACER <mahanasty@mail.ru>
2021-01-26 16:55:57 +0300

    create functions

--- src/index.js
+++ src/index.js
@@ -1,12 +1,31 @@
 
 exports.min = function min (array) {
-  return 0;
-}
+    if(array.length==0||array==undefined) {
+        return 0;
+    }
+
+    let minNumber = (a, b) => {
+        return a < b ? a : b;
+    };
+    let min=array.reduce(minNumber);
+    return min;
+};
 
 exports.max = function max (array) {
-  return 0;
-}
+    if(array.length ==0 || array === undefined)return 0;
+    let maxNumber = (a, b) => {
+            return a > b ? a : b;
+        };
+    let max = array.reduce(maxNumber);
+    return max;
+};
+
 
 exports.avg = function avg (array) {
-  return 0;
-}
+    if(array.length !==0||array !==undefined) {
+        let  total = array.reduce((acc, c) => acc + c, 0);
+        let res= total / array.length;
+        return res;
+    } return 0;
+};
+
